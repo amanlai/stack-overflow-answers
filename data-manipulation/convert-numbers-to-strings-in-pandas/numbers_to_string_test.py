370e8ce19aff1967fafee643f518a8a2ba75a829
unknown <manlai.lp@gmail.com>
2023-06-03 12:16:34 -0700

    reformat

--- data-manipulation/convert-numbers-to-strings-in-pandas/numbers_to_string_test.py
+++ data-manipulation/convert-numbers-to-strings-in-pandas/numbers_to_string_test.py
@@ -23,7 +23,10 @@ df.rename(columns=str).to_dict('records')
 # The code used to produce the performance plots:
 
 import numpy as np
+import matplotlib.pyplot as plt
 from perfplot import plot
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(15,5), facecolor='white')
+plt.sca(ax1)
 plot(
     setup=lambda n: pd.Series(np.random.default_rng().integers(0, 100, size=n)),
     kernels=[lambda s: s.astype(str), lambda s: s.astype("string"), lambda s: s.apply(str), lambda s: s.map(str), lambda s: s.map(repr)],
@@ -32,6 +35,7 @@ plot(
     xlabel='Number of rows',
     title='Converting a single column into string dtype',
     equality_check=lambda x,y: np.all(x.eq(y)));
+plt.sca(ax2)
 plot(
     setup=lambda n: pd.DataFrame(np.random.default_rng().integers(0, 100, size=(n, 100))),
     kernels=[lambda df: df.astype(str), lambda df: df.astype("string"), lambda df: df.applymap(str), lambda df: df.apply(lambda col: col.map(repr))],
