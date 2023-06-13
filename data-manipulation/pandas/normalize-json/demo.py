7b735ee6cb62e2d2f30551b3cf59b1b83b719059
unknown <manlai.lp@gmail.com>
2023-06-12 19:08:04 -0700

    normalize-json update demo

--- data-manipulation/pandas/normalize-json/demo.py
+++ data-manipulation/pandas/normalize-json/demo.py
@@ -1,9 +1,18 @@
-# deserialize json into a python data structure
 import json
 import pandas as pd
 
+
+# deserialize json into a python data structure
+with open('EUR_JPY_H8.json') as data_file:
+    data = json.load(data_file)
+
+# normalize the list of nested dicts
+df1 = pd.json_normalize(data)
+
+
+# deserialize json into a python data structure
 with open('my_data.json', 'r') as f:
     data = json.load(f)
 
 # normalize the python data structure
-df = pd.json_normalize(data, record_path=['price', 'mid'], meta=[['price', 'time']], record_prefix='mid.')
+df2 = pd.json_normalize(data, record_path=['price', 'mid'], meta=[['price', 'time']], record_prefix='mid.')
