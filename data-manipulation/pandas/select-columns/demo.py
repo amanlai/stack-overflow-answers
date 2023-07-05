dcdcd99d1e53e7c2bab6d93a7cfce33ff80baed6
unknown <manlai.lp@gmail.com>
2023-07-05 09:10:56 -0700

    update demo

--- data-manipulation/pandas/select-columns/demo.py
+++ data-manipulation/pandas/select-columns/demo.py
@@ -1,9 +1,15 @@
 import pandas as pd
 
-df = pd.DataFrame({'a': [1, 1, 1, 1], 'b': [2, 2, 1, 0], 'c': [3, 3, 1, 0]})
+df = pd.DataFrame({
+    'a': [1, 1, 1, 1], 
+    'b': [2, 2, 1, 0], 
+    'c': [3, 3, 1, 0]
+})
 cols = ['a', 'b']
 
+
 df1 = df[cols]
+print(df1)
 
 
 ####################################################
@@ -11,9 +17,34 @@ df1 = df[cols]
 
 cols = ['a', 'b', 'f']
 df1 = df.loc[:, df.columns.isin(cols)]   # <----- OK
+print(df1)
 
 
 ####################################################
 
+
 cols = ['a', 'b', 'f']
 df1 = df.filter(cols)
+print(df1)
+
+
+####################################################
+
+
+df1 = df.take([0,2], axis=1)
+print(df1)
+
+
+####################################################
+
+
+df1 = df.xs(pd.Index(['a', 'b']), axis=1)
+print(df1)
+
+
+df2 = df.xs('col1', level=1, axis=1)
+print(df2)
+
+# can select specific columns as well
+df3 = df.xs(pd.MultiIndex.from_tuples([('A', 'col1'), ('B', 'col2')]), axis=1)
+print(df3)
