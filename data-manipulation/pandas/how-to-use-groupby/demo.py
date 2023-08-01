4f9c6d071d6fe1d5a34c2c75c41947a1652324f1
unknown <manlai.lp@gmail.com>
2023-08-01 15:15:53 -0700

    add demo

--- data-manipulation/pandas/how-to-use-groupby/demo.py
+++ data-manipulation/pandas/how-to-use-groupby/demo.py
@@ -2,6 +2,10 @@ import pandas as pd
 from pandasql import sqldf
 from collections import ChainMap
 
+
+###########################################################################
+######################### NAMED AGGREGATION ###############################
+
 my_dict = {
     'Fruit': ['Apples', 'Apples', 'Apples', 'Apples', 'Apples', 'Oranges', 'Oranges', 'Oranges', 'Oranges', 'Oranges', 'Grapes', 'Grapes', 'Grapes', 'Grapes', 'Grapes'], 'Date': ['10/6/2016', '10/6/2016', '10/6/2016', '10/7/2016', '10/7/2016', '10/7/2016', '10/6/2016', '10/6/2016', '10/6/2016', '10/7/2016', '10/7/2016', '10/7/2016', '10/7/2016', '10/7/2016', '10/7/2016'], 
     'Name': ['Bob', 'Bob', 'Mike', 'Steve', 'Bob', 'Bob', 'Tom', 'Mike', 'Bob', 'Tony', 'Bob', 'Tom', 'Bob', 'Bob', 'Tony'], 
@@ -29,6 +33,7 @@ print(z)
 
 
 ###########################################################################
+##################### SUM, MEAN IN EACH GROUP #############################
 
 
 df = pd.DataFrame({'dummy': [0, 1, 1], 'A': range(3), 'B':range(1, 4), 'C':range(2, 5)})
@@ -56,6 +61,7 @@ print(z2)
 
 
 ###########################################################################
+####################### SUM IN EACH GROUP #################################
 
 
 x = df.groupby('dummy')[['A', 'B']].sum()
@@ -66,5 +72,34 @@ print(x)
 
 x = df.groupby("dummy", as_index=False)[['A', 'B']].sum()
 print(x)
+
+
+###########################################################################
+################### TOPMOST N IN EACH GROUP ###############################
+
+df = pd.DataFrame({'id':[1,1,1,2,2,2,2,3,4], 'value':[1,2,3,1,2,3,4,1,1]})
+N = 2
+
+x = df.groupby('id').head(N)
+print(x)
+
+df1 = df.groupby('id', as_index=False).nth[:N]
+print(df1)
+
+df2 = df.groupby('id', as_index=False).nth([0,2])
+print(df2)
+
+
+df1 = df.sort_values(by=['id', 'value'], ascending=[True, False])
+df1 = df1.groupby('id', as_index=False).nth[:N]
+print(df1)
+
+
+msk = df.groupby('id')['value'].rank(method='first', ascending=False) <= N
+df1 = df[msk]
+print(df1)
+
+y = df.loc[msk, 'value']
+print(y)
 
 
