4c6fe621413aab056e8801ce7318f523372ec85b
unknown <manlai.lp@gmail.com>
2023-08-13 19:17:33 -0700

    fix code

--- machine-learning-related/compute-z-score/timeit_test.py
+++ machine-learning-related/compute-z-score/timeit_test.py
@@ -10,7 +10,7 @@ t1 = min(timeit.repeat("pd.DataFrame(StandardScaler().fit_transform(df))",
                        globals=globals(), number=100)) / 5                   # 0.11218072000192478
 t2 = min(timeit.repeat("df.apply(stats.zscore)", 
                        globals=globals(), number=100)) / 5                   # 2.476688000001013
-t3 = min(timeit.repeat("pd.DataFrame(stats.zscore(df))", 
+t3 = min(timeit.repeat("stats.zscore(df)", 
                        globals=globals(), number=100)) / 5                   # 0.044902299996465445
 t4 = min(timeit.repeat("df.sub(df.mean()).div(df.std(ddof=0))", 
                        globals=globals(), number=100)) / 5                   # 0.028368819993920623
