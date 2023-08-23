cb034e70b6c714003bf7e0784aa059bfae7723a5
unknown <manlai.lp@gmail.com>
2023-08-22 23:21:43 -0700

    add convenience func

---
+++ machine-learning-related/compute-confidence-interval-in-linear-regression/conf_int.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+from scipy import stats
+from sklearn.linear_model import LinearRegression
+
+
+def get_conf_int(alpha, lr, X, y):
+    
+    """
+    Returns (1-alpha) 2-sided confidence intervals
+    for sklearn.LinearRegression coefficients
+    as a pandas DataFrame
+    """
+    
+    coefs = np.r_[[lr.intercept_], lr.coef_]
+    X_aux = X.copy()
+    X_aux.insert(0, 'const', 1)
+    dof = -np.diff(X_aux.shape)[0]
+    mse = np.sum((y - lr.predict(X)) ** 2) / dof
+    var_params = np.diag(np.linalg.inv(X_aux.T.dot(X_aux)))
+    t_val = stats.t.isf(alpha/2, dof)
+    gap = t_val * np.sqrt(mse * var_params)
+
+    return pd.DataFrame({
+        'lower': coefs - gap, 'upper': coefs + gap
+    }, index=X_aux.columns)
