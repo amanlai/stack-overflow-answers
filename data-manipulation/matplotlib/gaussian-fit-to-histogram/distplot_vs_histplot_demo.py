750b44cf4740a1ba0b508a9ae21ae93c3d3855e3
unknown <manlai.lp@gmail.com>
2023-08-25 21:42:21 -0700

    add second figure creator

---
+++ data-manipulation/matplotlib/gaussian-fit-to-histogram/distplot_vs_histplot_demo.py
@@ -0,0 +1,18 @@
+import numpy as np
+import seaborn as sns
+
+# sample data
+x = np.random.default_rng(0).normal(1, 4, size=500) * 0.1
+
+fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(10,4))
+
+# left subplot
+sns.distplot(x, kde=False, fit=stats.norm, ax=ax1)
+ax1.set_title('Using distplot')
+
+# right subplot
+sns.histplot(x, stat='density', color='#1f77b4', alpha=0.4, edgecolor='none', ax=ax2)
+add_fit_to_histplot(x, fit=stats.norm, ax=ax2)
+ax2.set_title('Using histplot+fit');
+
+fig.savefig('distplot_vs_histplot.png')
