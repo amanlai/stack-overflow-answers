6b1274671c6951b6571884cb6ad646eb8d5e69bc
amanlai <manlai.lp@gmail.com>
2023-10-11 15:39:46 -0700

    expand funcs

--- data-manipulation/pandas/get-length-of-lists/perfplot_tester.py
+++ data-manipulation/pandas/get-length-of-lists/perfplot_tester.py
@@ -6,10 +6,23 @@ import matplotlib.pyplot as plt
 
 random.seed(365)
 
+def f1(ser):
+    return ser.str.len()
+
+def f2(ser):
+    return ser.map(len)
+
+def f3(ser):
+    return list(map(len, ser.tolist()))
+
+def f4(ser):
+    return [len(x) for x in ser]
+
+
 plt.figure(figsize=(9,5), facecolor='white')
 perfplot.plot(
     setup=lambda n: pd.Series([random.sample(string.ascii_letters, random.randint(1, 20)) for _ in range(n)]),
-    kernels=[lambda ser: ser.str.len(), lambda ser: ser.map(len), lambda ser: list(map(len, ser.tolist())), lambda ser: [len(x) for x in ser]],
+    kernels=[f1, f2, f3, f4],
     labels=["ser.str.len()", "ser.map(len)", "list(map(len, ser.tolist()))", "[len(x) for x in ser]"],
     n_range=[2**k for k in range(21)],
     xlabel='Length of dataframe',
